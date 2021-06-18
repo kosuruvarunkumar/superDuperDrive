17cc0d09b37d283a742d5c3022c5d142f6e62cde
kosuruvarunkumar <kosuruvarunkumar@gmail.com>
2021-06-18 18:58:21 +0530

    few corrections in the templates, implemented notes

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NotesMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NotesMapper.java
@@ -1,10 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
+import com.udacity.jwdnd.course1.cloudstorage.models.NoteForm;
 import com.udacity.jwdnd.course1.cloudstorage.models.Notes;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -13,8 +11,16 @@ public interface NotesMapper {
     @Select("select * from notes where userid = #{userId}")
     List<Notes> getAllNotesOfAUser(Integer userId);
 
-    @Insert("insert into notes(notetitle, notesdescription, userid) values(#{notesTitle}," +
-            "#{notesDescription}, #{userId})")
+    @Insert("insert into notes(notetitle, notedescription, userid) values(#{noteTitle}," +
+            "#{notesDescription}, #{userid})")
     @Options(useGeneratedKeys = true,keyProperty = "id")
     int addNote(Notes note);
+
+    @Delete("delete from notes where noteid= #{noteid}")
+    void deleteNote(Integer noteid);
+
+    @Update("update notes set notetitle=#{noteTitle}, notedescription=#{noteDescription} where " +
+            "noteid=#{noteId}")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    int updateNote(NoteForm newNote);
 }
